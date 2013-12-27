74801ef79be3bd265c757deae44377e53cf24276
Burak Yigit Kaya <ben@byk.im>
2013-12-27 16:09:19 +0200

    Fix PEP8 errors

--- pngcanvas.py
+++ pngcanvas.py
@@ -4,12 +4,18 @@
 __version__ = "1.0.3"
 __author__ = "Rui Carmo (http://the.taoofmac.com)"
 __license__ = "MIT"
-__contributors__ = ["http://collaboa.weed.rbse.com/repository/file/branches/pgsql/lib/spark_pr.rb"], ["Eli Bendersky"], ["Dave Griffith"]
+__contributors__ = (
+    ["http://collaboa.weed.rbse.com/repository/"
+     "file/branches/pgsql/lib/spark_pr.rb"],
+    ["Eli Bendersky"],
+    ["Dave Griffith"],
+)
 
 import struct
 import zlib
 
 SIGNATURE = struct.pack("8B", 137, 80, 78, 71, 13, 10, 26, 10)
+
 
 def blend(c1, c2):
     """Alpha blends two colors, using the alpha given by c2"""
@@ -36,36 +42,36 @@ def gradient_list(start, end, steps):
 
 
 class PNGCanvas:
-
-    def __init__(self, width, height, bgcolor=bytearray([0xff,0xff,0xff,0xff]), color=bytearray([0,0,0,0xff])):
-        self.width   = width
-        self.height  = height
-        self.color   = color #rgba
+    def __init__(self, width, height,
+                 bgcolor=bytearray([0xff, 0xff, 0xff, 0xff]),
+                 color=bytearray([0, 0, 0, 0xff])):
+        self.width = width
+        self.height = height
+        self.color = color  # rgba
         self.bgcolor = bgcolor
-        self.canvas  = bytearray(self.bgcolor * 4 * width * height)
-
+        self.canvas = bytearray(self.bgcolor * 4 * width * height)
 
     def _offset(self, x, y):
         """Helper for internal data"""
         return y * self.width * 4 + x * 4
 
-
     def point(self, x, y, color=None):
         """Set a pixel"""
-        if x < 0 or y < 0 or x > self.width - 1 or y > self.height - 1: return
-        if color == None:
+        if x < 0 or y < 0 or x > self.width - 1 or y > self.height - 1:
+            return
+        if color is None:
             color = self.color
         o = self._offset(x, y)
-        self.canvas[o:o + 3] = blend(self.canvas[o:o + 3],bytearray(color))
-
+        self.canvas[o:o + 3] = blend(self.canvas[o:o + 3], bytearray(color))
 
     def rect_helper(self, x0, y0, x1, y1):
         """Rectangle helper"""
         x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
-        if x0 > x1: x0, x1 = x1, x0
-        if y0 > y1: y0, y1 = y1, y0
+        if x0 > x1:
+            x0, x1 = x1, x0
+        if y0 > y1:
+            y0, y1 = y1, y0
         return [x0, y0, x1, y1]
-
 
     def vertical_gradient(self, x0, y0, x1, y1, start, end):
         """Draw a vertical gradient"""
@@ -75,12 +81,10 @@ class PNGCanvas:
             for y in range(y0, y1 + 1):
                 self.point(x, y, grad[y - y0])
 
-
     def rectangle(self, x0, y0, x1, y1):
         """Draw a rectangle"""
         x0, y0, x1, y1 = self.rect_helper(x0, y0, x1, y1)
-        self.polyline([[x0, y0],[x1, y0],[x1, y1],[x0, y1],[x0, y0]])
-
+        self.polyline([[x0, y0], [x1, y0], [x1, y1], [x0, y1], [x0, y0]])
 
     def filled_rectangle(self, x0, y0, x1, y1):
         """Draw a filled rectangle"""
@@ -88,7 +92,6 @@ class PNGCanvas:
         for x in range(x0, x1 + 1):
             for y in range(y0, y1 + 1):
                 self.point(x, y, self.color)
-
 
     def copy_rect(self, x0, y0, x1, y1, dx, dy, destination):
         """Copy (blit) a rectangle onto another part of the image"""
@@ -98,7 +101,6 @@ class PNGCanvas:
                 d = destination._offset(dx + x - x0, dy + y - y0)
                 o = self._offset(x, y)
                 destination.canvas[d:d + 4] = self.canvas[o:o + 4]
-
 
     def blend_rect(self, x0, y0, x1, y1, dx, dy, destination, alpha=0xff):
         """Blend a rectangle onto the image"""
@@ -114,7 +116,7 @@ class PNGCanvas:
         """Draw a line using Xiaolin Wu's antialiasing technique"""
         # clean params
         x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
-        if y0>y1:
+        if y0 > y1:
             y0, y1, x0, x1 = y1, y0, x1, x0
         dx = x1 - x0
         if dx < 0:
@@ -143,7 +145,7 @@ class PNGCanvas:
         # main loop
         self.point(x0, y0)
         e_acc = 0
-        if dy > dx: # vertical displacement
+        if dy > dx:  # vertical displacement
             e = (dx << 16) / dy
             for i in range(y0, y1 - 1):
                 e_acc_temp, e_acc = e_acc, (e_acc + e) & 0xFFFF
@@ -168,48 +170,50 @@ class PNGCanvas:
             self.point(x0, y0 + 1, intensity(self.color, (0xFF-w)))
         self.point(x1, y1)
 
-
     def polyline(self, arr):
         """Draw a set of lines"""
         for i in range(0, len(arr) - 1):
             self.line(arr[i][0], arr[i][1], arr[i + 1][0], arr[i + 1][1])
 
-
     def dump(self):
         """Dump the image data"""
         scanlines = bytearray()
         for y in range(self.height):
-            scanlines.append('\0') # filter type 0 (None)
+            scanlines.append('\0')  # filter type 0 (None)
             #print y * self.width * 4, (y+1) * self.width * 4
             #print self.canvas[y * self.width * 4:(y+1) * self.width * 4]
-            scanlines.extend(self.canvas[(y * self.width * 4):((y + 1) * self.width * 4)])
+            scanlines.extend(
+                self.canvas[(y * self.width * 4):((y + 1) * self.width * 4)]
+            )
         # image represented as RGBA tuples, no interlacing
         return SIGNATURE + \
-            self.pack_chunk('IHDR', struct.pack("!2I5B",self.width,self.height, 8, 6, 0, 0, 0)) + \
+            self.pack_chunk('IHDR', struct.pack("!2I5B",
+                                                self.width, self.height,
+                                                8, 6, 0, 0, 0)) + \
             self.pack_chunk('IDAT', zlib.compress(str(scanlines), 9)) + \
             self.pack_chunk('IEND', '')
-
 
     def pack_chunk(self, tag, data):
         """Pack a PNG chunk for serializing to disk"""
         to_check = tag + data
-        return struct.pack("!I", len(data)) + to_check + struct.pack("!I", zlib.crc32(to_check) & 0xFFFFFFFF)
+        return (struct.pack("!I", len(data)) + to_check +
+                struct.pack("!I", zlib.crc32(to_check) & 0xFFFFFFFF))
 
-
-    def load(self,f):
+    def load(self, f):
         """Load a PNG image"""
         assert f.read(8) == SIGNATURE
         for tag, data in self.chunks(f):
             if tag == "IHDR":
-                ( width,
-                  height,
-                  bitdepth,
-                  colortype,
-                  compression, filter, interlace ) = struct.unpack("!2I5B", data)
-                self.width  = width
+                (width,
+                 height,
+                 bitdepth,
+                 colortype,
+                 compression, filter, interlace) = struct.unpack("!2I5B", data)
+                self.width = width
                 self.height = height
                 self.canvas = bytearray(self.bgcolor * width * height)
-                if (bitdepth,colortype,compression, filter, interlace) != (8, 6, 0, 0, 0):
+                if (bitdepth, colortype, compression,
+                        filter, interlace) != (8, 6, 0, 0, 0):
                     raise TypeError('Unsupported PNG format')
             # we ignore tRNS for the moment
             elif tag == 'IDAT':
@@ -232,29 +236,28 @@ class PNGCanvas:
                         self.point(x, y, rgba[j:j + 4])
                         j = j + 4
 
-
     def defilter(self, cur, prev, filtertype, bpp=3):
         """Decode a chunk"""
-        if filtertype == 0: # No filter
+        if filtertype == 0:  # No filter
             return cur
-        elif filtertype == 1: # Sub
+        elif filtertype == 1:  # Sub
             xp = 0
             for xc in range(bpp, len(cur)):
                 cur[xc] = (cur[xc] + cur[xp]) % 256
                 xp = xp + 1
-        elif filtertype == 2: # Up
+        elif filtertype == 2:  # Up
             for xc in range(len(cur)):
                 cur[xc] = (cur[xc] + prev[xc]) % 256
-        elif filtertype == 3: # Average
+        elif filtertype == 3:  # Average
             xp = 0
             for xc in range(len(cur)):
                 cur[xc] = (cur[xc] + (cur[xp] + prev[xc])/2) % 256
                 xp = xp + 1
-        elif filtertype == 4: # Paeth
+        elif filtertype == 4:  # Paeth
             xp = 0
             for i in range(bpp):
                 cur[i] = (cur[i] + prev[i]) % 256
-            for xc in range(bpp,len(cur)):
+            for xc in range(bpp, len(cur)):
                 a = cur[xp]
                 b = prev[xc]
                 c = prev[xp]
@@ -274,8 +277,7 @@ class PNGCanvas:
                 raise TypeError('Unrecognized scanline filter type')
         return cur
 
-
-    def chunks(self,f):
+    def chunks(self, f):
         """Split read PNG image data into chunks"""
         while 1:
             try:
@@ -287,7 +289,7 @@ class PNGCanvas:
                 return
             if zlib.crc32(tag + data) != crc:
                 raise IOError
-            yield [tag,data]
+            yield [tag, data]
 
 
 if __name__ == '__main__':
@@ -295,11 +297,12 @@ if __name__ == '__main__':
     width = 512
     height = 512
     print "Creating Canvas..."
-    c = PNGCanvas(width,height)
+    c = PNGCanvas(width, height)
     c.color = bytearray([0xff, 0, 0, 0xff])
     c.rectangle(0, 0, width - 1, height - 1)
     print "Generating Gradient..."
-    c.vertical_gradient(1, 1, width - 2, height - 2,[0xff, 0, 0, 0xff],[0x20, 0, 0xff, 0x80])
+    c.vertical_gradient(1, 1, width - 2, height - 2,
+                        [0xff, 0, 0, 0xff], [0x20, 0, 0xff, 0x80])
     print "Drawing Lines..."
     c.color = [0, 0, 0, 0xff]
     c.line(0, 0, width - 1, height - 1)
